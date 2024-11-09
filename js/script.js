f434a6bac59d12b521306aea54873dcef394f884
Jan Bil <64773378+Dzanek309@users.noreply.github.com.>
2024-11-09 11:19:37 +0100

    v0.1.4 - nav style and logic

--- js/script.js
+++ js/script.js
@@ -1,31 +1,4 @@
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: '.swiper-scrollbar',
-//     },
-// });
-
-// var map = L.map('map').setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//     .openPopup();
+function mobileMenu() {
+    let x = document.querySelector(".nav__link-wrapper-mobile");
+    x.classList.toggle('visible--flex');
+}
