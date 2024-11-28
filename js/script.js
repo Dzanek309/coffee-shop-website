24b6172a78525ecec4907cb884483abd6102e4f8
Jan Bil <64773378+Dzanek309@users.noreply.github.com.>
2024-11-28 19:35:51 +0100

    v0.1.7 - fixed issues with nav (mobile + desktop)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
+let mobile_nav_switch = document.querySelector(".nav__link-wrapper-mobile");
 function mobileMenu() {
-    let x = document.querySelector(".nav__link-wrapper-mobile");
-    x.classList.toggle('visible--flex');
+    mobile_nav_switch.classList.toggle('visible--flex');
 }
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 900 && mobile_nav_switch.classList.contains('visible--flex')) {
+        mobile_nav_switch.classList.toggle('visible--flex');
+    }
+});
