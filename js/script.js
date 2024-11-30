a22008e1c3a7f2bc49c985489ecd8aabab6bc695
Jan Bil <64773378+Dzanek309@users.noreply.github.com.>
2024-11-30 19:56:36 +0100

    v0.2.7 - visual changes

--- js/script.js
+++ js/script.js
@@ -7,4 +7,22 @@ window.addEventListener('resize', () => {
     if (window.innerWidth > 900 && mobile_nav_switch.classList.contains('visible--flex')) {
         mobile_nav_switch.classList.toggle('visible--flex');
     }
+
+    if (window.innerWidth > 900) {
+        swiper.params.slidesPerView = 3;
+    } else if (window.innerWidth > 500) {
+        swiper.params.slidesPerView = 2;
+    } else {
+        swiper.params.slidesPerView = 1;
+    }
+    swiper.update();
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
